a9eefe2cbec9bbc3265f96b61dce5d117faf2af7
Pooja <poojarajput9516@gmail.com>
2023-03-29 17:21:17 +0530

    addition

--- first.java
+++ first.java
@@ -4,5 +4,9 @@ class Main{
       public static void main(String args[]) {
          System.out.println("hello world");
          System.out.println("this is first file");
+         int a = 10;
+         int b = 20;
+         int c = (a+b);
+        System.out.println(c);
      }
 }
